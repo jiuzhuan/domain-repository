62bc8b3dcad956dcdc8d13dfd1bb15452260b1c2
jz <jiuzhuan@gmail.com>
2024-07-13 09:37:14 +0800

    membership DTO

---
+++ src/main/java/com/github/jiuzhuan/domain/repository/tangle/VIP/Points.java
@@ -0,0 +1,11 @@
+package com.github.jiuzhuan.domain.repository.tangle.VIP;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Points {
+
+    public static void main(String[] args) {
+
+    }
+}
